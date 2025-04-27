eeabc20da4d1f4f37bf90d7e752284d2f27b6921
Vinayak Khavare <vinayakkhavare62@gmail.com>
2025-04-27 12:34:50 +0530

    Removed proxies and testing files

--- test.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-response = requests.post(
-    "http://localhost:8000/chat",
-    headers={"X-Forwarded-For": "8.8.8.8"},
-    json={"message": "Find labor lawyers"}
-)
-print(response.json())
